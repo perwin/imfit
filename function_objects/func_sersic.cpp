43192ce26a349723cf1580b542df9af2ec001e1e
Peter Erwin <erwin@mpe.mpg.de>
2012-03-29 20:06:02 +0200

    29 Mar 2012: Various changes to SConstruct file to allow for universal Intel binary creation on Mac OS X (32-bit + 64-bit), and also to force static linking to the GSL library (since GCC tends to link to the shared-library version even when the static version is present). Further fleshing-out of imfit_howto.tex.
    Added MacArthur+2003 approximation for calculating Sersic b_n for n < 0.36 to
    func_sersic.cpp and func_gen-sersic.cpp.
    
    Added python/ directory with preliminary Python code.

--- function_objects/func_sersic.cpp
+++ function_objects/func_sersic.cpp
@@ -47,6 +47,12 @@ const int  SUBSAMPLE_R = 10;
 
 const char Sersic::className[] = "Sersic";
 
+const double  A0_M03 = 0.01945;
+const double  A1_M03 = -0.8902;
+const double  A2_M03 = 10.95;
+const double  A3_M03 = -19.67;
+const double  A4_M03 = 13.43;
+
 
 /* ---------------- CONSTRUCTOR ---------------------------------------- */
 
@@ -97,10 +103,15 @@ void Sersic::Setup( double params[], int offsetIndex, double xc, double yc )
   cosPA = cos(PA_rad);
   sinPA = sin(PA_rad);
   n2 = n*n;
-  /* The following approximation for b_n is good for all n > 0.36 */
-  bn = 2*n - 0.333333333333333 + 0.009876543209876543/n
-       + 0.0018028610621203215/n2 + 0.00011409410586365319/(n2*n)
-       - 7.1510122958919723e-05/(n2*n2);
+  if (n > 0.36) {
+    // Ciotti & Bertin (1999) approximation, good for all n > 0.36
+    bn = 2*n - 0.333333333333333 + 0.009876543209876543/n
+         + 0.0018028610621203215/n2 + 0.00011409410586365319/(n2*n)
+         - 7.1510122958919723e-05/(n2*n2);
+  } else {
+    // MacArthur et al. (2003) approximation for n < 0.36
+    bn = A0_M03 + A1_M03*n + A2_M03*n2 + A3_M03*n2*n + A4_M03*n2*n2;
+  }
   invn = 1.0 / n;
 }
 
