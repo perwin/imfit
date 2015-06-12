f708142dcb8dc31f6cb043378cd44117deb652f4
Peter Erwin <erwin@mpe.mpg.de>
2015-06-12 16:19:20 +0200

    Adding dispatch_solver.h/cpp to repo

---
+++ dispatch_solver.h
@@ -0,0 +1,24 @@
+/** @file
+ * \brief Code for calling specific solvers (minimization algorithms)
+ *
+ * 
+ *
+ */
+
+#ifndef _DISPATCH_SOLVER_H_
+#define _DISPATCH_SOLVER_H_
+
+#include <string>
+#include "model_object.h"
+#include "param_struct.h"   // for mp_par structure
+#include "solver_results.h"
+
+
+/// Function which handles selecting and calling appropriate solver
+int DispatchToSolver( int solverID, int nParametersTot, int nFreeParameters, int nPixelsTot,
+					double *parameters, mp_par *parameterInfo, ModelObject *modelObj, double fracTolerance,
+					bool paramLimitsExist, int verboseLevel, SolverResults *solverResults,
+					string& solverName );
+
+
+#endif /* _DISPATCH_SOLVER_H_ */
